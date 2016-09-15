c85d31a68147fee3e309b515322f33b043794693
Cody Condon <codycondon50@gmail.com>
2016-09-14 20:39:51 -0400

    add fetch request and attempt fetch function, add fetched results controller

---
+++ DreamLister/ItemCell.swift
@@ -0,0 +1,20 @@
+//
+//  ItemCell.swift
+//  DreamLister
+//
+//  Created by Cody Condon on 2016-09-14.
+//  Copyright © 2016 Cody Condon. All rights reserved.
+//
+
+import UIKit
+
+class ItemCell: UITableViewCell {
+
+    
+    @IBOutlet weak var thumb: UIImageView!
+    @IBOutlet weak var title: UILabel!
+    @IBOutlet weak var price: UILabel!
+    @IBOutlet weak var details: UILabel!
+    
+    
+}
