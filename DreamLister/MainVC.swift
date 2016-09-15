f7f07f4723146a9e0ca470758d14472b4d0c0e99
Cody Condon <codycondon50@gmail.com>
2016-09-15 14:35:57 -0400

    add comments for clarity

--- DreamLister/MainVC.swift
+++ DreamLister/MainVC.swift
@@ -15,6 +15,7 @@ class MainVC: UIViewController, UITableViewDelegate, UITableViewDataSource, NSFe
     @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var segment: UISegmentedControl!
     
+    //
     var controller: NSFetchedResultsController<Item>!
     
     override func viewDidLoad() {
@@ -36,20 +37,33 @@ class MainVC: UIViewController, UITableViewDelegate, UITableViewDataSource, NSFe
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if let sections = controller.sections {
+            let sectionInfo = sections[section]
+            return sectionInfo.numberOfObjects
+        }
+        
         return 0
     }
     
     func numberOfSections(in tableView: UITableView) -> Int {
+        if let sections = controller.sections {
+            return sections.count
+        }
         return 0
     }
     
     func attemptFetch() {
+        // what item to fetch
         let fetchRequest: NSFetchRequest<Item> = Item.fetchRequest()
+        // sort these based on time stamp
         let dateSort = NSSortDescriptor(key: "created", ascending: false)
+        //perform the sort action
         fetchRequest.sortDescriptors = [dateSort]
         
+        //creating the controller
         let controller = NSFetchedResultsController(fetchRequest: fetchRequest, managedObjectContext: context, sectionNameKeyPath: nil, cacheName: nil)
         
+        // go fetch item 
         do {
             try controller.performFetch()
         } catch {
@@ -58,6 +72,7 @@ class MainVC: UIViewController, UITableViewDelegate, UITableViewDataSource, NSFe
         }
     }
     
+    //whenever the tableView is about to update, this will start to listen for changes and will handle that for you
     func controllerWillChangeContent(_ controller: NSFetchedResultsController<NSFetchRequestResult>) {
         tableView.beginUpdates()
     }
@@ -66,8 +81,9 @@ class MainVC: UIViewController, UITableViewDelegate, UITableViewDataSource, NSFe
         tableView.endUpdates()
     }
     
-    
+    // this is going to be listening for when we make a change, whether its insertion, deletion modification etc.. swtich
     func controller(_ controller: NSFetchedResultsController<NSFetchRequestResult>, didChange anObject: Any, at indexPath: IndexPath?, for type: NSFetchedResultsChangeType, newIndexPath: IndexPath?) {
+        
         switch(type) {
         case.insert:
             if let indexPath = newIndexPath {
@@ -81,22 +97,20 @@ class MainVC: UIViewController, UITableViewDelegate, UITableViewDataSource, NSFe
             break
         case.update:
             if let indexPath = indexPath {
-                let cell = tableView.cellForRow(at: indexPath) as? ItemCell
+                let cell = tableView.cellForRow(at: indexPath) as! ItemCell
                 //update the cell data
             }
             break
         case.move:
             if let indexPath = indexPath {
-                tableView.deleteRows(at: [indexPath], with: .fade )
+                tableView.deleteRows(at: [indexPath], with: .fade)
             }
-            if let indexPath = newIndexPath {
+            if let indexPath = indexPath {
                 tableView.insertRows(at: [indexPath], with: .fade)
             }
             break
         }
     }
-    
-    
     
     
     
